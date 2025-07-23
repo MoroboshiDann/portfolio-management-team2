81627233392c897d223e04445366c0b2f1093a51
jackie <moroboshidan.me@gmail.com>
2025-07-23 08:14:32 +0000

    add start server code

--- src/app.js
+++ src/app.js
@@ -3,9 +3,25 @@ const bodyParser = require('boday-parser');
 const app = express();
 const mysql = require('mysql2');
 
+const PORT = 3000;
+
+app.use(express.json());
+
 const db = mysql.createConnection({
     host : 'localhost',
     user : 'student',
     password : '123123',
     database : 'databasename'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database', err);
+        process.exit(1);
+    }
+    console.log('Connected to database');
+    // start server 
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
 });
