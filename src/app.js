4ddb01853ef0fcee1dd06b58fde09d676aba31c5
jackie <moroboshidan.me@gmail.com>
2025-07-25 03:39:26 +0000

    fix front end app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,73 @@
-const express = require('express');
-const bodyParser = require('boday-parser');
-const app = express();
-const mysql = require('mysql2');
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { TransactionProvider } from "./context/useTrans";
+import "./App.css";
 
-const PORT = 3000;
+// Lazy load components for performance optimization
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const PortfolioForm = lazy(() => import("./components/PortfolioForm"));
+const TransactionForm = lazy(() => import("./components/TransactionForm"));
+const TransactionReconciliation = lazy(() => import("./components/TransRec"));
+const Navbar = lazy(() => import("./components/Navbar")); // Import Navbar
 
-app.use(express.json());
+function App() {
+  useEffect(() => {
+    document.title = "PortfolioPulse"; // Set the title here
+  }, []);
+  // State Management for portfolio data
+  const [portfolios, setPortfolios] = useState([]);
 
-const db = mysql.createConnection({
-    host : '172.30.1.42',
-    user : 'root',
-    password : 'n3u3da!',
-    database : 'portfoliopulse'
-});
+  // Function to handle adding a new portfolio
+  const addPortfolio = (portfolio) => {
+    setPortfolios([...portfolios, portfolio]);
+  };
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database', err);
-        process.exit(1);
-    }
-    console.log('Connected to database');
-    // start server 
-    app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-    });
-});
+  const updatePortfolio = (updatedPortfolio) => {
+    setPortfolios(
+      portfolios.map((portfolio) =>
+        portfolio.name === updatedPortfolio.name ? updatedPortfolio : portfolio
+      )
+    );
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <TransactionProvider>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Navbar />
+            <div className="container mx-auto p-6">
+              {/* Implement Suspense for lazy-loaded components */}
+              <Routes>
+                <Route
+                  path="/"
+                  element={<Dashboard portfolios={portfolios} />}
+                />
+                <Route
+                  path="/portfolio"
+                  element={
+                    <PortfolioForm
+                      portfolios={portfolios}
+                      addPortfolio={addPortfolio}
+                      updatePortfolio={updatePortfolio}
+                    />
+                  }
+                />
+                <Route
+                  path="/transaction"
+                  element={<TransactionForm />} // No longer need to pass addTransaction and transactions
+                />
+                <Route
+                  path="/reconciliation"
+                  element={<TransactionReconciliation />} // No longer need to pass transactions
+                />
+              </Routes>
+            </div>
+          </Suspense>
+        </TransactionProvider>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
